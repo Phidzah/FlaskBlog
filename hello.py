08f6f32b462ec75afab23dd56c45f433bcee9e60
Phidza <phillip.jaricha@gmail.com>
2023-12-13 20:12:41 +0200

    tweeked comments in hello.py

--- hello.py
+++ hello.py
@@ -5,13 +5,14 @@ app = Flask(__name__)
 # in Jinja templating you can use filters on the information that you pass
 # Examples of such filters are:
 # {{ name|capitalize}} to Capitalize the first letter of name
-# {{ name|upper}} and Lower to convert the name to Uppercase/Lowercase
+# {{ name|upper}} to convert the name to Uppercase
+# {{ name|lower}} to convert the name to Lowercase
 # {{ stuff|safe}} safe is used to pass html (only done when you know its you who is passing safe html tags)
 # {{ stuff|striptags}} maybe in comments that are to be stored in a database to deter html injection
 # {{ stuff|trim}} remove trailing spaces from the end of string variable
 # {{ stuff|title}} Capitalize the first letter of every word in the stuff variable
 
-# Jinja also allows us to pass lists and dictionaries to our html templates
+# Jinja also allows us to pass variables, lists and dictionaries to our html templates
 @app.route('/')
 def index():
 	username = "phidza"
@@ -21,10 +22,6 @@ def index():
 		username=username,
 		stuff=stuff,
 		list_of_favourites=list_of_favourites)
-
-
-
-
 
 # localhost:5000/user/Phillip
 @app.route("/user/<name>")
@@ -36,6 +33,10 @@ def user(name):
 @app.errorhandler(404)
 def page_not_found(e):
 	return render_template('404.html'),404
+
+@app.errorhandler(500)
+def page_not_found(e):
+	return render_template('500.html'),500
 
 if '__name___' == '__main__':
 	app.run(debug=True)
